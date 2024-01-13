4d9f8db46bf22b93a3560fab86603b8915f823fd
mcande21 <candersoan@gocva.com>
2024-01-13 15:18:53 -0500

    LinkedIn Leanring Modules

---
+++ 02_02_begin/stack.py
@@ -0,0 +1,48 @@
+"""
+Python Data Structures - A Game-Based Approach
+Stack class
+Robin Andrews - https://compucademy.net/
+"""
+
+
+class Stack:
+    # constructor for the stack
+    # this is the code that gets run when it is called
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        # A nempty lists defines itself as false
+        # so is_empty returns true
+        return not self.items
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[-1]
+
+    def size(self):
+        return len(self.items)
+
+    def __str__(self):
+        return str(self.items)
+
+
+if __name__ == "__main__":
+    s = Stack()
+    print(s)
+    print(s.is_empty())
+    s.push(3)
+    print(s)
+    s.push(7)
+    s.push(5)
+    print(s)
+    print(s.pop())
+    print(s)
+    print(s.peek())
+    print(s)
+    print(s.size())
